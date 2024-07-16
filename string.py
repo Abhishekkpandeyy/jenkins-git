4a0ce8f8b70d79e5398fa4e52878020866f06bd4
Abhishekkpandeyy <118623849+Abhishekkpandeyy@users.noreply.github.com>
2024-07-16 21:03:47 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+
+# string concetination
+
+var1= "abhi"
+print(var1)
+var2 = 'Pandey'
+print (var2)
+print (var1+var2)
+print (var1+' ' +var2)
+
+# print (var1 + 3)   error as string can not be added with number
+print (var1 + '3')   # here var1 is added with string as 3
+
+print (var1 * 2)   # here var1 variable will print 2 times
